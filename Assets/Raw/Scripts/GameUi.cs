cb6ad7d73e3cdae3347b8552df907926d4b9148a
alvinsat <alvinsatrianugraha@yahoo.co.id>
2021-10-12 17:07:08 +0700

    Init data

---
+++ Assets/Raw/Scripts/GameUi.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUi : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+    
+    [Header("Target scale")]
+    Vector3 initialScale;
+    [SerializeField]
+    float scaleFactor;
+    [SerializeField]
+    float maxZoomIn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialScale = target.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BtnZoomIn() {
+        if (target.transform.localScale.x < maxZoomIn) {
+            Vector3 newScale = target.localScale;
+            newScale.x += scaleFactor;
+            newScale.y += scaleFactor;
+            newScale.z += scaleFactor;
+            target.transform.localScale = newScale;
+        }
+    }
+
+    public void BtnZoomOut() {
+        if (target.transform.localScale.x > initialScale.x) {
+            Vector3 newScale = target.localScale;
+            newScale.x -= scaleFactor;
+            newScale.y -= scaleFactor;
+            newScale.z -= scaleFactor;
+            target.transform.localScale = newScale;
+        }
+    }
+
+    public void BtnZoomOutMax() {
+        // Force zooming out
+        // TODO anim zooming out
+        target.transform.localScale = initialScale;
+    }
+
+}
---
+++ Assets/Raw/Scripts/ShowReco.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class ShowReco : MonoBehaviour
+{
+    private CloudRecoBehaviour mCloudRecoBehaviour;
+    private bool mIsScanning = false;
+    private string mTargetMetadata = "";
+    public ImageTargetBehaviour ImageTargetTemplate;
+    [SerializeField]
+    UnityEngine.UI.Text txtObjPos;
+    GameObject objs;
+
+    // Register cloud reco callbacks
+    void Awake()
+    {
+        mCloudRecoBehaviour = GetComponent<CloudRecoBehaviour>();
+        mCloudRecoBehaviour.RegisterOnInitializedEventHandler(OnInitialized);
+        mCloudRecoBehaviour.RegisterOnInitErrorEventHandler(OnInitError);
+        mCloudRecoBehaviour.RegisterOnUpdateErrorEventHandler(OnUpdateError);
+        mCloudRecoBehaviour.RegisterOnStateChangedEventHandler(OnStateChanged);
+        mCloudRecoBehaviour.RegisterOnNewSearchResultEventHandler(OnNewSearchResult);
+        objs = ImageTargetTemplate.transform.GetChild(0).gameObject;
+    }
+
+
+    void Update()
+    {
+        txtObjPos.text = objs.transform.position.x.ToString()+ " "
+            + objs.transform.position.y.ToString() + " "+objs.transform.position.z.ToString();
+    }
+
+    //Unregister cloud reco callbacks when the handler is destroyed
+    void OnDestroy()
+    {
+        mCloudRecoBehaviour.UnregisterOnInitializedEventHandler(OnInitialized);
+        mCloudRecoBehaviour.UnregisterOnInitErrorEventHandler(OnInitError);
+        mCloudRecoBehaviour.UnregisterOnUpdateErrorEventHandler(OnUpdateError);
+        mCloudRecoBehaviour.UnregisterOnStateChangedEventHandler(OnStateChanged);
+        mCloudRecoBehaviour.UnregisterOnNewSearchResultEventHandler(OnNewSearchResult);
+    }
+
+    public void OnInitialized(TargetFinder targetFinder)
+    {
+        Debug.Log("Cloud Reco initialized");
+    }
+    public void OnInitError(TargetFinder.InitState initError)
+    {
+        Debug.Log("Cloud Reco init error " + initError.ToString());
+    }
+    public void OnUpdateError(TargetFinder.UpdateState updateError)
+    {
+        Debug.Log("Cloud Reco update error " + updateError.ToString());
+    }
+
+    public void OnStateChanged(bool scanning)
+    {
+        mIsScanning = scanning;
+        if (scanning)
+        {
+            // clear all known trackables
+            var tracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+            tracker.GetTargetFinder<ImageTargetFinder>().ClearTrackables(false);
+        }
+    }
+
+    // Here we handle a cloud target recognition event
+    public void OnNewSearchResult(TargetFinder.TargetSearchResult targetSearchResult)
+    {
+        TargetFinder.CloudRecoSearchResult cloudRecoSearchResult =
+            (TargetFinder.CloudRecoSearchResult)targetSearchResult;
+        // do something with the target metadata
+        mTargetMetadata = cloudRecoSearchResult.MetaData;
+        if (ImageTargetTemplate)
+        {
+            // enable the new result with the same ImageTargetBehaviour: 
+            ObjectTracker tracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+            tracker.GetTargetFinder<ImageTargetFinder>().EnableTracking(targetSearchResult, ImageTargetTemplate.gameObject);
+        }
+        // stop the target finder (i.e. stop scanning the cloud)
+        mCloudRecoBehaviour.CloudRecoEnabled = false;
+
+    }
+
+    void OnGUI()
+    {
+        // Display current 'scanning' status
+        GUI.Box(new Rect(100, 100, 200, 50), mIsScanning ? "Scanning" : "Not scanning");
+        // Display metadata of latest detected cloud-target
+        GUI.Box(new Rect(100, 200, 200, 50), "Metadata: " + mTargetMetadata);
+        // If not scanning, show button
+        // so that user can restart cloud scanning
+        if (!mIsScanning)
+        {
+            if (GUI.Button(new Rect(100, 300, 200, 50), "Restart Scanning"))
+            {
+                // Restart TargetFinder
+                mCloudRecoBehaviour.CloudRecoEnabled = true;
+            }
+        }
+    }
+}
